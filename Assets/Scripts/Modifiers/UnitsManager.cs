a63b0eb3b568cb1b88316147a3e3bb2138e3f52f
DESKTOP-CKD71D6\Utilisateur <tristan.charial@gmail.com>
2022-07-16 15:15:44 +0200

    units Maanager

--- Assets/Scripts/Modifiers/UnitsManager.cs
+++ Assets/Scripts/Modifiers/UnitsManager.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class UnitsManager : MonoBehaviour
 {
+    public int maxEnemyAmountOnScreen;
     public static UnitsManager Instance;
     public delegate void OnUnitDeath();
     public OnUnitDeath onUnitDeath;
 
+    public List<GameObject> enemyToCreateEachWave;
+    public int amountOfRemainingEnemy;
+
     void Awake()
     {
         Instance = this;   
+        enemyToCreateEachWave = new List<GameObject>();
     }
 
+    public void AddEnemyToWave(GameObject enemy)
+    {
+        enemyToCreateEachWave.Add(enemy);
+    }
+
+    public void StartWave()
+    {
+
+    }
 }
